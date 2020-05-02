eb9e3d6d6947b44f3119aa077a3e3912e67f4eff
JazminTeng <jazminteng@gmail.com>
2020-05-02 16:47:26 -0300

    consultas y casos

---
+++ src/models/Caso.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataType) => {
+
+    const modeloCasos = {
+        id: {
+            type: DataType.INTEGER,
+            primaryKey:true,
+            autoIncrement: true
+        },
+        fechaInicio: {
+            type: DataType.DATE,
+            allowNull: false,
+            validate: {
+                isDate: true
+            },
+            defaultValue: newDate() 
+        }
+    }
+
+    // NOMBRE TABLA, Y SUS FILAS
+    const Caso = sequelize.define('Casos', modeloCasos);
+
+    // Relacion uno a muchos
+    Tasks.associate = (models) => {
+        Caso.belongsTo(models.Users);
+    };
+
+    return Caso;
+
+};
