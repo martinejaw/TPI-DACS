4536c061b9108901a519d5ce5fe042d2eab4af0a
martinejaw <martinezequieljaworski@gmail.com>
2020-05-15 11:10:38 -0300

    Added Hospital/Caso domain

---
+++ src/domain/models/Caso.js
@@ -0,0 +1,8 @@
+class Caso{
+    constructor(id, fechaInicio) {
+        this.id = id;
+        this.fechaInicio = fechaInicio;
+    }
+}
+
+module.exports = Caso;
---
+++ src/domain/models/Hospital.js
@@ -0,0 +1,9 @@
+class Hospital{
+    constructor(nombre, ciudad, calle) {
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.calle = calle;
+    }
+}
+
+module.exports = Hospital;
