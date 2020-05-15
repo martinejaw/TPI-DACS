b5f6ddbe9c7e0ff8a0ae22b27975aa70befd6497
bravinjuan <bravinjuan@gmail.com>
2020-05-15 11:10:26 -0300

    Modelo de dominio de paciente y medico

---
+++ src/domain/models/Medico.js
@@ -0,0 +1,8 @@
+class Medico {
+    constructor(matricula, nombre, apellido) {
+      this.matricula = matricula;
+      this.nombre = nombre;
+      this.apellido = apellido;
+    }
+  }
+module.exports = Medico;
