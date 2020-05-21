4405bf20582caa0a0dec530d0ccea5922f28438a
bravinjuan <bravinjuan@gmail.com>
2020-05-21 14:51:49 -0300

    otra vez noooo

--- src/dal/repositories/PacienteRepository.js
+++ src/dal/repositories/PacienteRepository.js
@@ -6,19 +6,6 @@ class PacienteRepository extends BaseRepository {
     super(db, "Pacientes");
   }
   
-  /*get(dni) {
-    return this._db.models[this.entity].findOne({ where: { dni } });
-  }
-
-  update(dni, entity) {
-    delete entity.createdAt;
-    delete entity.updatedAt;
-    return this._db.models[this.entity].update(entity, { where: { dni } });
-  }
-
-  delete(dni) {
-    return this._db.models[this.entity].destroy({ where: { dni } });
-  }*/
   /*getAll() {
     return this._db.models[this.entity].findAll({ include: [ db.models.Consultas ] });
   }*/
