d871d8c6bed64a16c8dc09736b081d25a292fff8
bravinjuan <bravinjuan@gmail.com>
2020-05-21 13:30:33 -0300

    solucionado conflictos 2

--- src/dal/entities/Hospital.js
+++ src/dal/entities/Hospital.js
@@ -2,11 +2,8 @@ module.exports = (sequelize, DataType) => {
 
     // NOMBRE TABLA, Y SUS FILAS
     const Hospital = sequelize.define('Hospitales', {
-<<<<<<< HEAD
         CUIT: {
-=======
-        cuit: {
->>>>>>> ff7867093243797591ce596c3b5e60510cbf04d6
+
             type: DataType.INTEGER,
             primaryKey:true,
             autoIncrement: true
@@ -20,15 +17,12 @@ module.exports = (sequelize, DataType) => {
         }
     });
 
-<<<<<<< HEAD
-=======
     Hospital.associate = (models) => {
         Hospital.belongsTo(models.Direcciones,{as:'DireccionHospital'});
         //Hospital.hasMany(models.Recursos);
         Hospital.belongsToMany(models.Medicos, {through: 'Medicos_Hospitales'});
     };
     
->>>>>>> ff7867093243797591ce596c3b5e60510cbf04d6
     return Hospital;
 
 }
