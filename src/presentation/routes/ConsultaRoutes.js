4c06c7525b2e1aa52d7db44a840bfa84a2bf1f99
Exequiel Schuster <exe97as@gmail.com>
2020-05-24 17:39:24 -0300

    Consultas sin responder sin test

--- src/presentation/routes/ConsultaRoutes.js
+++ src/presentation/routes/ConsultaRoutes.js
@@ -7,12 +7,9 @@ module.exports = ( { ConsultaController } ) => {
     router.get('/', ConsultaController.getConsultas.bind(ConsultaController));
     router.post("/", ConsultaController.createConsulta.bind(ConsultaController));
 
-<<<<<<< HEAD
     router.get('/consultasinresponder/:medicodni', ConsultaController.getConsultaSinResponder.bind(ConsultaController));
     router.post("/consultasinresponder/:medicodni", ConsultaController.getConsultaSinResponder.bind(ConsultaController));
-=======
     router.post("/api", ConsultaController.recibirConsulta.bind(ConsultaController));
->>>>>>> e37959a305c2e716f978795b2420c81caf72b9d3
 
     return router;
 }
