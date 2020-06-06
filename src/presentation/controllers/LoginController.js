3371d8960139f4092e7db4ce5f192e3e7dbd692f
martinejaw <martinezequieljaworski@gmail.com>
2020-06-06 12:50:11 -0300

    Conexion Login comenzada

--- src/presentation/controllers/LoginController.js
+++ src/presentation/controllers/LoginController.js
@@ -1,11 +1,21 @@
+const path = require('path')
+
 class LoginController {
     constructor({LoginService}){
         this._loginService = LoginService;
     }
 
+    async loginFront(req,res){
+        await this._loginService.validarRegistro(req.body)
+            .then(usuarioValidado => res.render(path.join(__dirname+'/../views/login')))//status(200).json(calles))
+            .catch(error => {
+                res.status(404).json({msg: error.message});  
+            });
+    }
     async login(req,res){
         await this._loginService.validarRegistro(req.body)
-            .then(calles => res.status(200).json(calles))
+            .then(usuarioValidado => 
+                res.status(200).json(usuarioValidado))
             .catch(error => {
                 res.status(404).json({msg: error.message});  
             });
--- src/presentation/routes/LoginRoutes.js
+++ src/presentation/routes/LoginRoutes.js
@@ -4,6 +4,7 @@ const { Router } = require("express");
 module.exports = ( { LoginController } ) => {
     const router = Router();
     router.post('/', LoginController.login.bind(LoginController));
+    router.get('/', LoginController.loginFront.bind(LoginController));
     
     return router;
 }
