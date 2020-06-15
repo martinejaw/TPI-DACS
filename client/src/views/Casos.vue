8df7066853b748f29f27912e383b3e690d250389
martinejaw <martinezequieljaworski@gmail.com>
2020-06-14 21:18:32 -0300

    Dialogo de error Alta Casos

--- client/src/views/Casos.vue
+++ client/src/views/Casos.vue
@@ -14,37 +14,47 @@
         aria-label="Search"
       />
       <div class="col-md-6"></div>
+
+      <!--Form nuevo caso-->
       <v-dialog v-model="dialog" persistent max-width="600px">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn class="btn btn-outline-success my-2 my-sm-0 col-md-2" color="primary"
-        dark v-bind="attrs" v-on="on">Agregar Caso</v-btn>
-      </template>
-      <v-card>
-        <v-card-title>
-          <span class="headline">Nuevo Caso</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12">
-                <v-text-field v-model="dni" label="DNI Paciente*" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-select v-model="estado"
-                :items="['Grave', 'Gravisimo', 'Muerto', 'Semi Muerto']" label="Estado*"
-                required></v-select>
-              </v-col>
-            </v-row>
-          </v-container>
-          <small>*Campos Obligatorios</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="dialog = false">Cerrar</v-btn>
-          <v-btn color="blue darken-1" text @click="dialog = false; altaCaso()">Agregar</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            class="btn btn-outline-success my-2 my-sm-0 col-md-2"
+            color="primary"
+            dark
+            v-bind="attrs"
+            v-on="on"
+          >Agregar Caso</v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            <span class="headline">Nuevo Caso</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field v-model="dni" label="DNI Paciente*" required></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-select
+                    v-model="estado"
+                    :items="['Grave', 'Gravisimo', 'Muerto', 'Estable']"
+                    label="Estado*"
+                    required
+                  ></v-select>
+                </v-col>
+              </v-row>
+            </v-container>
+            <small>*Campos Obligatorios</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="dialog = false">Cerrar</v-btn>
+            <v-btn color="blue darken-1" text @click="dialog = false; altaCaso()">Agregar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </form>
     <hr class="my-1" />
     <div class="btn-group d-flex" role="group">
@@ -52,6 +62,19 @@
       <a class="btn btn-outline-secondary w-100">Estado</a>
       <a class="btn btn-outline-secondary w-100">Fecha</a>
     </div>
+
+    <!--Form error-->
+    <v-row justify="center">
+      <v-dialog v-model="errorBool" persistent max-width="500">
+        <v-card>
+          <v-card-title class="headline"> {{ error }} </v-card-title>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" text @click="errorBool = false">Enterado</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
 
     <hr class="my-2" />
     <!-- inicio de la tabla de datos -->
@@ -67,20 +90,23 @@
                       <div class="cuerpoDatos col-sm-8">
                         <h2 class="card-title">ID caso: {{ caso.id }}</h2>
                         <p class="card-text">
-                          <strong>DNI Paciente:</strong> {{ caso.PacienteDni }}
+                          <strong>DNI Paciente:</strong>
+                          {{ caso.PacienteDni }}
                         </p>
                         <p class="card-text">
-                          <strong>Fecha:</strong> {{ caso.fechaInicio }}
+                          <strong>Fecha:</strong>
+                          {{ caso.fechaInicio }}
                         </p>
                         <p>
                           <strong>Estado:</strong>
                           <span class="badge bg-primary"></span>
                           <span class="badge bg-info"></span>
                           <span class="badge bg-warning"></span>
-                          <span v-if="caso.estado == `Estable`" class="badge bg-success">
-                          {{ caso.estado }} </span>
-                          <span class="badge bg-success">
-                          {{ caso.estado }} </span>
+                          <span
+                            v-if="caso.estado == `Estable`"
+                            class="badge bg-success"
+                          >{{ caso.estado }}</span>
+                          <span v-else class="badge bg-danger">{{ caso.estado }}</span>
                         </p>
                       </div>
                       <div class="imagen col-sm-4 text-center">
@@ -118,24 +144,26 @@ export default {
     dialog: false,
     dni: 0,
     estado: '',
+    errorBool: false,
   }),
   mounted() {
     this.actualizarCasos();
   },
   methods: {
     altaCaso() {
-      console.log(this.estado);
       const url = `${cfg.Casos_URL}`;
       axios.post(url, { estado: this.estado, PacienteDni: this.dni, MedicoDni: 4100325 })
         .then((result) => {
           if (result.status === 200) {
             console.log('Error en el alta');
+            this.error = result.data.msg;
+            this.errorBool = true;
           } else {
             console.log('Caso cargado correctamente');
             this.actualizarCasos();
           }
         })
-        .catch((error) => { this.error = error.message; });
+        .catch((error) => { this.error = error.message; this.errorBool = true; });
     },
     actualizarCasos() {
       const url = `${cfg.Casos_URL}`;
@@ -143,7 +171,7 @@ export default {
         .then((result) => {
           this.casos = result.data;
         })
-        .catch((error) => { this.error = error.message; });
+        .catch((error) => { this.error = error.message; this.errorBool = true; });
     },
     watch: {
       error: () => {
