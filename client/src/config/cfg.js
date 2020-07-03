22d1062b61c6fd6b62eec7a1e0b7d4ad55a4949e
bravinjuan <bravinjuan@gmail.com>
2020-07-03 19:35:09 -0300

    congifuracion para deploy de front separado

--- client/src/config/cfg.js
+++ client/src/config/cfg.js
@@ -1,16 +1,16 @@
 const cfg = {
 
-  API_URL: 'http://localhost:3000/',
-  VAL_URL: 'http://localhost:3000/login',
-  Recursos_URL: 'http://localhost:3000/recurso',
-  Consultas_URL: 'http://localhost:3000/consulta',
-  ConsultasSinResponder_URL: 'http://localhost:3000/consulta/consultasinresponder',
-  ConsultasRespondidas_URL: 'http://localhost:3000/consulta/consultasrespondidas',
-  Casos_URL: 'http://localhost:3000/caso',
-  Diagnostico_URL: 'http://localhost:3000/consulta/diagnosticar',
-  PartesMedicos_URL: 'http://localhost:3000/partemedico',
-  Pruebas_URL: 'http://localhost:3000/prueba',
-  Pedidos_URL: 'http://localhost:3000/recurso/pedirRecursos',
+  API_URL: 'http://54.90.6.116:3000/',
+  VAL_URL: 'http://54.90.6.116:3000/login',
+  Recursos_URL: 'http://54.90.6.116:3000/recurso',
+  Consultas_URL: 'http://54.90.6.116:3000/consulta',
+  ConsultasSinResponder_URL: 'http://54.90.6.116:3000/consulta/consultasinresponder',
+  ConsultasRespondidas_URL: 'http://54.90.6.116:3000/consulta/consultasrespondidas',
+  Casos_URL: 'http://54.90.6.116:3000/caso',
+  Diagnostico_URL: 'http://54.90.6.116:3000/consulta/diagnosticar',
+  PartesMedicos_URL: 'http://54.90.6.116:3000/partemedico',
+  Pruebas_URL: 'http://54.90.6.116:3000/prueba',
+  Pedidos_URL: 'http://54.90.6.116:3000/recurso/pedirRecursos',
 };
 
 module.exports = cfg;
