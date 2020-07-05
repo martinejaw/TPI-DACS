ce520f50fe5f12a86be715651e8206592f90cf71
bravinjuan <bravinjuan@gmail.com>
2020-07-05 20:30:09 -0300

    A

--- client/src/config/cfg.js
+++ client/src/config/cfg.js
@@ -1,17 +1,17 @@
 const cfg = {
 
-  API_URL: "http://54.237.73.187:3000/",
-  VAL_URL: "http://54.237.73.187:3000/login",
-  Recursos_URL: "http://54.237.73.187:3000/recurso",
-  Consultas_URL: "http://54.237.73.187:3000/consulta",
-  ConsultasSinResponder_URL: "http://54.237.73.187:3000/consulta/consultasinresponder",
-  ConsultasRespondidas_URL: "http://54.237.73.187:3000/consulta/consultasrespondidas",
-  Casos_URL: "http://54.237.73.187:3000/caso",
-  Diagnostico_URL: "http://54.237.73.187:3000/consulta/diagnosticar",
-  PartesMedicos_URL: "http://54.237.73.187:3000/partemedico",
-  Pruebas_URL: "http://54.237.73.187:3000/prueba",
-  Pedidos_URL: "http://54.237.73.187:3000/recurso/pedirRecursos",
-  Medicos_URL: "http://54.237.73.187:3000/medico",
+  API_URL: 'http://54.237.73.187:3000/',
+  VAL_URL: 'http://54.237.73.187:3000/login',
+  Recursos_URL: 'http://54.237.73.187:3000/recurso',
+  Consultas_URL: 'http://54.237.73.187:3000/consulta',
+  ConsultasSinResponder_URL: 'http://54.237.73.187:3000/consulta/consultasinresponder',
+  ConsultasRespondidas_URL: 'http://54.237.73.187:3000/consulta/consultasrespondidas',
+  Casos_URL: 'http://54.237.73.187:3000/caso',
+  Diagnostico_URL: 'http://54.237.73.187:3000/consulta/diagnosticar',
+  PartesMedicos_URL: 'http://54.237.73.187:3000/partemedico',
+  Pruebas_URL: 'http://54.237.73.187:3000/prueba',
+  Pedidos_URL: 'http://54.237.73.187:3000/recurso/pedirRecursos',
+  Medicos_URL: 'http://54.237.73.187:3000/medico',
 
 };
 
