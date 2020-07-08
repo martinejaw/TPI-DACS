0ca47e9c0e433a1bd4af40772e99cb5c6822be28
martinejaw <martinezequieljaworski@gmail.com>
2020-07-08 12:16:16 -0300

    Crear y leer el token

--- src/presentation/controllers/LoginController.js
+++ src/presentation/controllers/LoginController.js
@@ -1,17 +1,14 @@
-const path = require('path')
-const jwt = require('jwt-simple')
-const cfg = require('../../config/environments/cfg')
-
 class LoginController {
     constructor({LoginService}){
         this._loginService = LoginService;
     }
 
     async login(req,res){
+        
+
         await this._loginService.validarRegistro(req.body)
-            .then(usuarioValidado => {  
-                if(usuarioValidado!=false){
-                    const token = jwt.encode(usuarioValidado, cfg.SECRET);
+            .then(token => {  
+                if(token != false){
                     res.status(200).json({token: token})
                 } else {
                     res.status(401).json({ message: "Acceso denegado" })
@@ -19,6 +16,8 @@ class LoginController {
             .catch(error => {
                 res.status(400).json({msg: error.message});  
             });
+
+        await this._loginService.decodeToken(1);
     }
 
     async register(req,res){
--- src/service/LoginService.js
+++ src/service/LoginService.js
@@ -1,11 +1,15 @@
 const BaseService = require("./base.service");
 const bcrypt = require("bcryptjs");
+const moment = require('moment')
+const cfg = require('../config/environments/cfg')
+const jwt = require('jwt-simple')
 
 class LoginService extends BaseService{
     constructor({ UnitOfWork, MedicoService }){
         super(UnitOfWork.CuentaRepository,"Cuenta");
         this._medicoService = MedicoService;
         this._adminRepository = UnitOfWork.AdministradorRepository;
+        this._token = null;
     }
 
     async validarRegistro(cuenta){
@@ -35,21 +39,95 @@ class LoginService extends BaseService{
                     nombreHospital = consulta.Hospitale.nombre;
                 }
                 
-                return {
-                    usuario: cuentaValida.usuario, 
-                    password: cuentaValida.password, 
+                const payload = {
                     rol: cuentaValida.rol, 
                     dni: dni, 
                     cuit: HospitalCuit, 
                     nombre: nombre,
-                    hospital: nombreHospital
-                };
+                    hospital: nombreHospital,
+                    iat: moment().unix(),
+                    exp: moment().add(3, 'hours').unix()
+                }
+
+                const token = jwt.encode(payload, cfg.SECRET);
+                this._token = token;
+                console.log(token);
+
+                // ######### Si tuvieramos que hacer a mano con algun codificador ##########
+                /* const header = {
+                    alg: "HS256",
+                    typ: "JWT"
+                } */
+                // const unsignedToken = jwt.encode(header, cfg.SECRET) + '.' + jwt.encode(payload, cfg.SECRET);
+                // const signature = jwt.encode(unsignedToken, cfg.SECRET, 'RS256') ;
+                // const token = unsignedToken + '.' + signature ;
+
+                return token;
                 
             } else {
                 return false;
             }
         }
     }
+
+
+    async decodeToken(authHeader) {
+        const decodedPayload = new Promise((resolve, reject) => {
+            try {
+                /* SI TUVIERAMOS QUE HACER A MANO
+                if (!authHeader) {
+                    reject({
+                        status: 401,
+                        message: 'Token vacio'
+                    });
+                }
+
+                const token = authHeader.split(' ')[1];
+                const token = token.split('.');
+
+                const header = jwt.decode(token[0], cfg.SECRET);
+                const payload = jwt.decode(token[1], cfg.SECRET);
+
+                const unsignedToken = jwt.decode(token[2], cfg.SECRET, 'RS256');  // Es decir la firma / signature decodificada
+                unsignedToken = unsignedToken.split('.');
+
+                const headerFirma = unsignedToken[0];
+                const payloadFirma = unsignedToken[1];
+
+                if (header != headerFirma || payload != payloadFirma) {
+                    reject({
+                        status: 403,
+                        message: 'Token sin integridad'
+                    });
+                }   */
+
+                let token = header.split(' ')[1];
+
+                payload = jwt.decode(token, cfg.SECRET);
+
+
+                if (payload.exp > moment.unix()) {
+                    reject({
+                        status: 403,
+                        message: 'El token ha expirado'
+                    });
+                }
+
+                // Comprobar que el id de la sesion exista en el almacenamiento de sesiones
+
+                resolve(token);
+
+            } catch(err) {
+                reject({
+                    status: 403,
+                    message: err.message
+                });
+            }
+        })
+
+        return decodedPayload;
+    }
+
 }
 
 module.exports = LoginService;
